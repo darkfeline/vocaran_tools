65674328a15f576c96a00bf9fa4f1590c8b76e6c
Allen Li <darkfeline@abagofapples.com>
2012-05-14 16:13:38 -0700

    Adds move_songs.py hook to vocaran_tools.py
    Also adds help messages to action hooks in parser.

--- vocaran_tools.py
+++ vocaran_tools.py
@@ -5,20 +5,24 @@
 
 import argparse
 
-ACTIONS=(('-d', 'download'), 
-         ('-p', 'parse')
+ACTIONS=(('-d', 'download', 'download songs'),
+         ('-c', 'convert', 'convert song list ranks'),
+         ('-m', 'move', 'move songs')
         )
 
 parser = argparse.ArgumentParser(description='vocaran_tools utility.')
 parser.add_argument('args', nargs='*')
 group = parser.add_mutually_exclusive_group(required=True)
-for a, b in ACTIONS:
-    group.add_argument(a, dest='action', action='store_const', const=b)
+for a, b, c in ACTIONS:
+    group.add_argument(a, dest='action', action='store_const', const=b, help=c)
 args = parser.parse_args()
 
 if args.action == 'download':
     import dl
     dl.main(*args.args)
-elif args.action == 'parse':
+elif args.action == 'convert':
     import parse
     parse.main(*args.args)
+elif args.action == 'move':
+    import move_songs
+    move_songs.main()
