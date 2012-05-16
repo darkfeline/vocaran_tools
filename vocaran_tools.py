12c64c4cb3f14dc11c3b54d80171b398d4cc7b59
Allen Li <darkfeline@abagofapples.com>
2012-05-15 19:46:05 -0700

    Minor changes to vocaran_tools.py
    Changes initial comment to module docstring.
    Changes parser store constant to flag.

--- vocaran_tools.py
+++ vocaran_tools.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
 
-# vocaran_tools.py
-# CLI script
+"""
+vocaran_tools.py
+CLI script
+
+"""
 
 import argparse
 
-ACTIONS=(('-d', 'download', 'download songs'),
-         ('-c', 'convert', 'convert song list ranks'),
-         ('-m', 'move', 'move songs')
+ACTIONS=(('-d', '--download', 'download songs'),
+         ('-c', '--convert', 'convert song list ranks'),
+         ('-m', '--move', 'move songs')
         )
 
 parser = argparse.ArgumentParser(description='vocaran_tools utility.')
 parser.add_argument('args', nargs='*')
 group = parser.add_mutually_exclusive_group(required=True)
 for a, b, c in ACTIONS:
-    group.add_argument(a, dest='action', action='store_const', const=b, help=c)
+    group.add_argument(a, b, dest='action', action='store_const', const=a, 
+            help=c)
 args = parser.parse_args()
 
-if args.action == 'download':
+if args.action == '-d':
     import dl
     dl.main(*args.args)
-elif args.action == 'convert':
+elif args.action == '-c':
     import parse
     parse.main(*args.args)
-elif args.action == 'move':
+elif args.action == '-m':
     import move_songs
     move_songs.main()
