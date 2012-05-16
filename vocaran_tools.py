ab157dcc15cdbad05630ae466b5933da0e9cafb2
Allen Li <darkfeline@abagofapples.com>
2012-05-15 20:41:05 -0700

    Makes vocaran_tools.py __name__=="__main__"

--- vocaran_tools.py
+++ vocaran_tools.py
@@ -6,27 +6,29 @@ CLI script
 
 """
 
-import argparse
-
 ACTIONS=(('-d', '--download', 'download songs'),
          ('-c', '--convert', 'convert song list ranks'),
          ('-m', '--move', 'move songs')
         )
 
-parser = argparse.ArgumentParser(description='vocaran_tools utility.')
-parser.add_argument('args', nargs='*')
-group = parser.add_mutually_exclusive_group(required=True)
-for a, b, c in ACTIONS:
-    group.add_argument(a, b, dest='action', action='store_const', const=a, 
-            help=c)
-args = parser.parse_args()
+if __name__ == "__main__":
 
-if args.action == '-d':
-    import dl
-    dl.main(*args.args)
-elif args.action == '-c':
-    import parse
-    parse.main(*args.args)
-elif args.action == '-m':
-    import move_songs
-    move_songs.main(*args.args)
+    import argparse
+
+    parser = argparse.ArgumentParser(description='vocaran_tools utility.')
+    parser.add_argument('args', nargs='*')
+    group = parser.add_mutually_exclusive_group(required=True)
+    for a, b, c in ACTIONS:
+        group.add_argument(a, b, dest='action', action='store_const', const=a, 
+                help=c)
+    args = parser.parse_args()
+
+    if args.action == '-d':
+        import dl
+        dl.main(*args.args)
+    elif args.action == '-c':
+        import parse
+        parse.main(*args.args)
+    elif args.action == '-m':
+        import move_songs
+        move_songs.main(*args.args)
