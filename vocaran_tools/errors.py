e88b633723ba328858114fe06be576480d555acf
Allen Li <darkfeline@abagofapples.com>
2012-07-26 23:46:14 -0700

    Document Exceptions

--- vocaran_tools/errors.py
+++ vocaran_tools/errors.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
 class ExitException(Exception):
+
+    """Used to raise sys.exit()"""
+
     def __init__(self, code=0, *args):
         self.code = code
         super().__init__(*args)
 
+
+class FileNotAvailableException(Exception):
+
+    """Used when download file is not available"""
+
+
 class DependencyException(Exception):
-    pass
+
+    """Used when dependency is not met"""
+
 
 class DataException(Exception):
-    pass
 
-class FileNotAvailableException(DataException):
-    pass
+    """Base class for data-related exceptions"""
+
 
 class FileFormatError(DataException):
-    pass
+
+    """Used when there's a problem with file formatting"""
+
 
 class StructureException(DataException):
-    pass
+
+    """Used when there's a problem with data dir structure"""
