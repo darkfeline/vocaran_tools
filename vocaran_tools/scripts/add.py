0e9c258974e17e9545208f018faa8d80a1440aac
Allen Li <darkfeline@abagofapples.com>
2012-07-26 23:17:49 -0700

    Add exception handling

--- vocaran_tools/scripts/add.py
+++ vocaran_tools/scripts/add.py
@@ -6,7 +6,8 @@ add.py
 """
 
 from vocaran_tools.data import dm, parse
-from vocaran_tools.errors import StructureException, ExitException
+from vocaran_tools.errors import StructureException, FileFormatError
+from vocaran_tools.errors import ExitException
 
 def main(*args):
 
@@ -22,7 +23,7 @@ def main(*args):
 def add_main(week, file):
     try:
         slist = parse.read_list(file, ranks=True)
-    except StructureException as e:
+    except (StructureException, FileFormatError) as e:
         print(str(e))
         raise ExitException(1)
     slist.file = dm.get_songlist_path(week)
