df8100b34d291389603db9369a7f644cec8c15f0
Allen Li <darkfeline@abagofapples.com>
2012-07-26 20:42:16 -0700

    Add translate script

---
+++ vocaran_tools/scripts/translate.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+"""
+translate.py
+
+"""
+
+import os
+import os.path
+
+from vocaran_tools.data import parse
+from vocaran_tools.data import dm
+from vocaran_tools.vocaloidism import get_vocaloidism
+from vocaran_tools.errors import DataException
+
+def main(*args):
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('week', type='int')
+    parser.add_argument('-s', '--source', action='store', type='str')
+    args = parser.parse_args(args)
+
+    translate_main(args.week, args.source)
+
+def translate_main(week, source):
+    if source is None:
+        print('Getting Vocaloid HTML for week {}...'.format(str(week)))
+        src = 'src.tmp'
+        get_vocaloidism(src, week)
+    else:
+        print('Using {} as src'.format(source))
+        src = source
+    print('parsing src...')
+    ranks = parse.parse_vocaloidism(src)
+    print('checking parsed links...')
+    if parse.checklinks(ranks):
+        raise DataException('parse_vocaloidism links is incomplete.  ' + 
+            'Check source and/or parse_vocaloidism', parse.checklinks(ranks))
+    print('reading song list data...')
+    slist = dm.get_songlist(week)
+    print('converting ranks...')
+    parse.convert_list(slist, ranks)
+    print('saving...')
+    slist.save()
+    if source is None and os.path.isfile('src.tmp'):
+        print('removing src.tmp...')
+        os.remove('src.tmp')
+    print('Done.')
+
+if __name__ == '__main__':
+    import sys
+    try:
+        main(*sys.argv[1:])
+    except DataException as e:
+        print(str(e))
+        sys.exit(1)
