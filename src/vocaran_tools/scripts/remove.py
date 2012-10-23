e60987e141d7e81cbd8ae591d207eedd68043177
Allen Li <darkfeline@abagofapples.com>
2012-10-22 21:51:20 -0400

    Clean up remove

--- src/vocaran_tools/scripts/remove.py
+++ src/vocaran_tools/scripts/remove.py
@@ -6,26 +6,18 @@ remove.py
 import os
 
 from vocaran_tools.data import dm
-from vocaran_tools.errors import StructureError, FileFormatError
 from vocaran_tools.errors import ExitException
+
 
 def main(*args):
 
     import argparse
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('week')
+    parser.add_argument('name')
     args = parser.parse_args(args)
 
-    remove_main(args.week)
-
-def remove_main(week):
-    try:
-        slist = dm.get_songlist(week)
-    except (StructureError, FileFormatError) as e:
-        print(str(e))
-        raise ExitException(1)
-    os.remove(slist.file)
+    os.remove(dm.get_songlist_path(args.name))
 
 if __name__ == '__main__':
     import sys
