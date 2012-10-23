1cc90aa506a91489a10e629ba4a216d0bf5763e9
Allen Li <darkfeline@abagofapples.com>
2012-10-22 21:49:18 -0400

    Clean up show

--- src/vocaran_tools/scripts/show.py
+++ src/vocaran_tools/scripts/show.py
@@ -1,12 +1,8 @@
-"""
-show.py
-
-"""
-
 import os
 
 from vocaran_tools.data import dm
 from vocaran_tools.errors import StructureError, ExitException
+
 
 def main(*args):
 
@@ -14,47 +10,46 @@ def main(*args):
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-r', '--raw', action='store_true', default=False)
-    parser.add_argument('week', nargs='?', type=int, default=None)
+    parser.add_argument('name', nargs='?', type=int, default=None)
     args = parser.parse_args(args)
 
     try:
-        show_main(args.week, raw=args.raw)
+        show_main(args.name, raw=args.raw)
     except StructureError as e:
         print(str(e))
         raise ExitException(1)
 
-def print_summary(slist):
-    print("{}, {}, {} entries".format(slist.week, slist.__class__.__name__,
-        len(slist)), end="")
+
+def print_header(slist):
+    print("{}, {} entries".format(slist.name, len(slist)), end="")
     if slist.done:
         print(", Done", end="")
     print()
 
-def show_main(week, raw=False):
-    if week is None:
+
+def show_main(name, raw=False):
+    if name is None:
         for x in dm.check_songlists():
             x = dm.get_songlist(x)
-            print_summary(x)
+            print_header(x)
     else:
-        path = dm.get_songlist_path(week)
-        if not os.path.isfile(path):
-            raise StructureError('{} is not a file.'.format(path))
+        x = dm.get_songlist(name)  # raises StructureError
         if raw:
+            path = dm.get_songlist_path(name)
             with open(path) as f:
                 for line in f:
                     print(line, end="")
         else:
-            x = dm.get_songlist(week)
-            print_summary(x)
+            print_header(x)
             for entry in x:
                 print('-' * 10)
-                print(
-"""ID:{}
+                print(_entry_template.format(*iter(entry))
+_entry_template = """ID:{}
 Name:{}
 Artist:{}
 Album:{}
 Comment:{}
-APIC:{}""".format(*iter(entry)))
+APIC:{}"""
 
 if __name__ == '__main__':
     import sys
